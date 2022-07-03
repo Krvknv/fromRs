8aae08ab69bfab905e41758b859511cfe6389d98
Krvknv <cristina.krivokoneva@yandex.ru>
2022-07-03 20:23:20 +0300

    feat:change sources.js

--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -1,12 +1,16 @@
 import './sources.css';
+import { TSources } from '../../../types/index';
+interface ISources {
+    draw(data: TSources[]): void;
+}
 
-class Sources {
-    draw(data) {
+class Sources implements ISources {
+    draw(data: TSources[]) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+        const sourceItemTemp = document.querySelector<HTMLTemplateElement>('#sourceItemTemp');
 
         data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
 
             sourceClone.querySelector('.source__item-name').textContent = item.name;
             sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
