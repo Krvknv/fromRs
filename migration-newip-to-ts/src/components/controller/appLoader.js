95fb74ab4776d76c610dc113ce7210dd70c0b284
Krvknv <cristina.krivokoneva@yandex.ru>
2022-07-03 19:48:56 +0300

    feat: change api key and link

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '2da02d42a0bf46708e6e5bdd24d846ed', // получите свой ключ https://newsapi.org/
         });
     }
 }
