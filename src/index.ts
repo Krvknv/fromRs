d37d8b6dcf3bbb03ec4a91fa698043585e8d66bb
Krvknv <cristina.krivokoneva@yandex.ru>
2022-07-07 21:01:30 +0300

    feat: create sidebar

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
 import './assets/styles/main.scss';
 import './assets/styles/style.css';
+
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
+
+const sliderYear = document.getElementById('slider-year');
+const sliderQuantity = document.getElementById('slider-quantity');
+
+noUiSlider.create(sliderYear, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        min: 0,
+        max: 100,
+    },
+});
+noUiSlider.create(sliderQuantity, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        min: 0,
+        max: 100,
+    },
+});
