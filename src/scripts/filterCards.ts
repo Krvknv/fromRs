dbab8518f8e11eeaa1a20ccd2cad66f8be6d6c68
Krvknv <cristina.krivokoneva@yandex.ru>
2022-07-14 18:55:27 +0300

    feat: create matal and color filters

--- src/scripts/filterCards.ts
+++ src/scripts/filterCards.ts
@@ -6,8 +6,7 @@ const cards = document.querySelector('.cards');
 export const formCollection = document.querySelectorAll('.collection .category__label .category__input');
 export const formMetal = document.querySelectorAll('.metal .category__label .category__input');
 const formColor = document.querySelectorAll('.color .category__label .category__input');
-const filteredCards: itemType[] = [];
-const rangeFilteredCardsArr = JSON.parse(localStorage.getItem('rangeFilteredCards'));
+
 export let filters: string[] = JSON.parse(localStorage.getItem('filters')) || [];
 
 export function showChosenFiltersCollection() {
@@ -41,6 +40,7 @@ export function showChosenFiltersColor() {
 export function chooseFilter(event: Event, value: string) {
     if ((event.target as HTMLInputElement).checked) {
         filters.push(value);
+        console.log(filters);
         localStorage.setItem('filters', JSON.stringify(filters));
 
         chooseFilteredCardsCollection();
@@ -48,6 +48,7 @@ export function chooseFilter(event: Event, value: string) {
         filters = filters.filter((item) => {
             return item !== value;
         });
+        console.log(filters);
 
         localStorage.setItem('filters', JSON.stringify(filters));
 
@@ -56,16 +57,21 @@ export function chooseFilter(event: Event, value: string) {
 }
 
 export function chooseFilteredCardsCollection() {
+    // console.log('flter');
     if (filters.length === 0) {
         cards.innerHTML = null;
         renderCard();
         return;
     }
 
+    const filteredCards: itemType[] = [];
+    const rangeFilteredCardsArr = JSON.parse(localStorage.getItem('rangeFilteredCards'));
     if (
         (filters.includes('gold') || filters.includes('silver') || filters.includes('pink gold')) &&
         (filters.includes('yellow') || filters.includes('grey') || filters.includes('pink'))
     ) {
+        console.log('color metal');
+
         for (const card of rangeFilteredCardsArr) {
             if (filters.includes(card.metal.toLowerCase()) && filters.includes(card.color.toLowerCase())) {
                 filteredCards.push(card);
@@ -85,6 +91,8 @@ export function chooseFilteredCardsCollection() {
             filters.includes('portugal')) &&
         (filters.includes('yellow') || filters.includes('grey') || filters.includes('pink'))
     ) {
+        console.log('collect color');
+
         for (const card of rangeFilteredCardsArr) {
             if (filters.includes(card.collection.toLowerCase()) && filters.includes(card.color.toLowerCase())) {
                 filteredCards.push(card);
@@ -105,6 +113,8 @@ export function chooseFilteredCardsCollection() {
             filters.includes('portugal')) &&
         (filters.includes('yellow') || filters.includes('grey') || filters.includes('pink'))
     ) {
+        console.log('collect color metal');
+
         for (const card of rangeFilteredCardsArr) {
             if (
                 filters.includes(card.collection.toLowerCase()) &&
@@ -128,8 +138,10 @@ export function chooseFilteredCardsCollection() {
             filters.includes('milan') ||
             filters.includes('portugal'))
     ) {
+        console.log('collect metal');
         for (const card of rangeFilteredCardsArr) {
             if (filters.includes(card.collection.toLowerCase()) && filters.includes(card.metal.toLowerCase())) {
+                console.log('in');
                 filteredCards.push(card);
             }
         }
@@ -141,6 +153,8 @@ export function chooseFilteredCardsCollection() {
         return;
     }
     if (filters.includes('gold') || filters.includes('silver') || filters.includes('pink gold')) {
+        console.log('metal');
+
         for (const card of rangeFilteredCardsArr) {
             if (filters.includes(card.metal.toLowerCase())) {
                 filteredCards.push(card);
@@ -154,6 +168,8 @@ export function chooseFilteredCardsCollection() {
         return;
     }
     if (filters.includes('yellow') || filters.includes('grey') || filters.includes('pink')) {
+        console.log('color');
+
         for (const card of rangeFilteredCardsArr) {
             if (filters.includes(card.color.toLowerCase())) {
                 filteredCards.push(card);
@@ -173,6 +189,16 @@ export function chooseFilteredCardsCollection() {
         filters.includes('milan') ||
         filters.includes('portugal')
     ) {
+        console.log('collect');
+        //  for (const filter of filters) {
+        //     for (const card of rangeFilteredCardsArr) {
+        //         if (card.collection.toLowerCase() === filter) {
+        //             console.log('bjkfdnjbkfndjkfd');
+        //             filteredCards.push(card);
+        //         }
+        //     }
+        // }
+
         for (const card of rangeFilteredCardsArr) {
             if (filters.includes(card.collection.toLowerCase())) {
                 filteredCards.push(card);
@@ -184,6 +210,13 @@ export function chooseFilteredCardsCollection() {
             sortFilteredCards(filteredCards);
         }
     }
+
+    console.log(filteredCards);
+    // if (filteredCards.length === 0) {
+    //     cards.innerHTML = '<span class="no-found">Sorry, there was no match</span>';
+    // } else {
+    //     sortFilteredCards(filteredCards);
+    // }
 }
 
 // export function chooseFilteredCardsMetal() {
