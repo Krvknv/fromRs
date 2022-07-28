c2f6a5d16b949a4c9cbdd35d832ce425cd06a00d
Krvknv <cristina.krivokoneva@yandex.ru>
2022-07-28 16:48:55 +0300

    init: start task

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Race",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new ESLintPlugin({ extensions: "ts" }),
+    new FaviconsWebpackPlugin("src/assets/image/svg/favicon.svg"),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.ts$/i, use: "ts-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        use: ["file-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".css"],
+  },
+};
