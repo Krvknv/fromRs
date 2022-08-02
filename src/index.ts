f7b0d8b554dc0c63b918cf4e504569cf1777735d
Krvknv <cristina.krivokoneva@yandex.ru>
2022-08-02 15:09:27 +0300

    feat: add start app

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import './style.css';
+
+import { startApp } from './js/app';
+
+window.addEventListener('DOMContentLoaded', startApp);
+
+// gameControls.addEventListener('click', (event) => {
+//     if ((event.target as HTMLElement).classList.contains('btn-a')) {
+//         console.log('a');
+//     }
+//     if ((event.target as HTMLElement).classList.contains('btn-b')) {
+//         console.log('b');
+//     }
+// });
